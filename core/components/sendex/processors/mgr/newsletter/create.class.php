14f2fca521819a86e5574d0bd51377cf9027924d
ershov-ilya <ershov.ilya@gmail.com>
2013-11-25 15:14:01 +0400

    Добавлены обновлённые файлы

---
+++ core/components/sendex/processors/mgr/newsletter/create.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Create an Item
+ */
+class sxNewsletterCreateProcessor extends modObjectCreateProcessor {
+	public $objectType = 'sxNewsletter';
+	public $classKey = 'sxNewsletter';
+	public $languageTopics = array('sendex');
+	public $permission = 'new_document';
+
+
+	/**
+	 * @return bool
+	 */
+	public function beforeSet() {
+		$alreadyExists = $this->modx->getObject('sxNewsletter', array(
+			'name' => $this->getProperty('name'),
+		));
+		if ($alreadyExists) {
+			$this->modx->error->addField('name', $this->modx->lexicon('sendex_item_err_ae'));
+		}
+
+		return !$this->hasErrors();
+	}
+
+}
+
+return 'sxNewsletterCreateProcessor';
